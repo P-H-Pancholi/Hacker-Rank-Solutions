960f5b0205596e6aa18ea19a21d518d5420a7b33
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-06-30 22:29:23 +0530

    Updated

--- Problem Solving/Flatland Space Station.py
+++ Problem Solving/Flatland Space Station.py
@@ -1,13 +1,13 @@
 n, m = map(int,input().split())
 list_of_indices = list(map(int,input().split()))
-cities = [city for city in range(n) if city not in list_of_indices]
 maximum = 0
 
-for city in cities:
-    diff_city_space = [abs(city - list_of_indices[i]) for i in range(m)]
-    minimum = min(diff_city_space)
-    if minimum > maximum:
-        maximum = minimum
+for city in range(n):
+	if city not in list_of_indices:
+		diff_city_space = [abs(city - list_of_indices[i]) for i in range(m)]
+		minimum = min(diff_city_space)
+		if minimum > maximum:
+			maximum = minimum
 
 
 
