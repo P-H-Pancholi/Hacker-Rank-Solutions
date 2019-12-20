9bbbe1d31d71a34b59206a68107f163e43c41f2f
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-12-20 18:36:38 +0530

    Fails in two test cases due to timeout

---
+++ Problem Solving/Matrix Layer Rotation.py
@@ -0,0 +1,32 @@
+m,n,r = map(int, input().split())
+matrix = []
+for _ in range(m):
+    matrix.append(list(input().split()))
+
+no_of_layers = min(m,n)//2
+
+for i in range(no_of_layers):
+    rotations_required = r % ( 2 * ( m + n - 4 * i ) - 4)
+    for rotation in range(rotations_required):
+
+        #Rotate Top Row
+        for j in range(i,n-i-1):
+            matrix[i][j], matrix[i][j+1] = matrix[i][j+1], matrix[i][j]
+
+        #Rotate Right column
+        for j in range(i,m-i-1):
+            matrix[j][n-i-1],matrix[j+1][n-i-1] = matrix[j+1][n-i-1], matrix[j][n-i-1]
+
+		#Rotate Bottom Row
+        for j in range(n-i-1,i,-1):
+            matrix[m-i-1][j], matrix[m-i-1][j-1] = matrix[m-i-1][j-1], matrix[m-i-1][j]
+
+        #Rotate Left Column
+        for j in range(m-i-1,i+1,-1):
+            matrix[j][i], matrix[j-1][i] = matrix[j-1][i], matrix[j][i]
+
+for i in range(m):
+    for j in range(n):
+        print(matrix[i][j],end = " ")
+        if j == n-1:
+            print("\n",end="")
