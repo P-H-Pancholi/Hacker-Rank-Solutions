f6538fb9167dcccc49b93b2d176c8d8813da85e6
MukundKulkarni <mukundkulkarni786@gmail.com>
2020-04-22 20:22:45 +0530

    Updated

--- Problem Solving/Dynamic Programming/Sherlock and Cost.py
+++ Problem Solving/Dynamic Programming/Sherlock and Cost.py
@@ -1,22 +1,14 @@
-def get_diff(arr):
-    diff = 0
-    for i in range(len(arr)-1):
-        diff += abs(arr[i] - arr[i+1])
 
-    return diff
 t = int(input())
 for _ in range(t):
     n = int(input())
     b = list(map(int,input().split()))
-    solution_matrix = []
-    max = 0
-    arr1 = b[::]
-    arr2 = b[::]
-    print(arr1)
-    print(arr2)
-    for i in range(n):
-        arr1[i] = 1
-        arr2[i] = b[i]
-        max = max(max, get_diff(arr1), get_diff(arr2))
 
-    print(max)
+
+    solution_matrix = [[0,0] for i in range(n)]
+
+    for i in range(1,n):
+        solution_matrix[i][1] = max(solution_matrix[i-1][0] + (abs(b[i] - 1)), solution_matrix[i-1][1] + (abs(b[i-1] - b[i])))
+        solution_matrix[i][0] = max(solution_matrix[i-1][1] + abs(1 - b[i-1]), solution_matrix[i-1][0] + abs((1 - 1)))
+
+    print(max(solution_matrix[n-1][0],solution_matrix[n-1][1]))
