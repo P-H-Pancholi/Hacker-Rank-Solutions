d8c4e6ff6ae4e00e7fd4d9ed4186a4b3128795ad
MukundKulkarni <mukundkulkarni786@gmail.com>
2020-04-27 12:15:08 +0530

    Updated

--- Problem Solving/Implementation/Ema's Supercomputer.py
+++ Problem Solving/Implementation/Ema's Supercomputer.py
@@ -1,60 +1,109 @@
+from copy import deepcopy
 n,m = map(int,input().split())
-grid = []
-for _ in range(n):
-    grid.append(list(str(input().strip())))
+MAT = [list(input()) for _ in range(n)]
+MAT1 = MAT
 
-#print(grid)
-max1 = 0
-max1_i = 0
-max1_j = 0
-max2 = 0
-max2_i = 0
-max2_j = 0
+def print_matrix(MAT1):
+    print("MATRIX")
+
+    for row in MAT1:
+        print("".join(row))
+
+
+def check_plus(i,j,n,m,MAT1):
+    cells_above = 0
+    cells_below = 0
+    cells_right = 0
+    cells_left = 0
+
+    #Cells above (i,j)
+    k = i
+    while k >= 0 and MAT1[k][j] == 'G':
+        cells_above += 1
+        k -= 1
+
+    #Cells below (i,j)
+    k = i
+    while k < n and MAT1[k][j] == 'G':
+        cells_below += 1
+        k += 1
+
+    #Cells right (i,j)
+    l = j
+    while l >= 0 and MAT1[i][l] == 'G':
+        cells_left += 1
+        l -= 1
+
+    #Cells left (i,j)
+    l = j
+    while l < m and MAT1[i][l] == 'G':
+        cells_right += 1
+        l += 1
+
+    max_plus = min(cells_left,cells_above,cells_below,cells_right)
+    print(max_plus)
+
+    return max_plus + 3 * (max_plus - 1)
+
+res1 = []
+res12 = []
+size1 = 0
 
 for i in range(n):
     for j in range(m):
-        print(grid[i][j],end="")
+        if MAT1[i][j] == 'B':
+            continue
+        res = check_plus(i,j,n,m,MAT)
+        if res > size1:
+            row,col = i,j
+            size1 = res
+            res1.append((res,i,j))
 
 
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == 'G':
-            for k in range(min(n,m)):
-                if i + k < n and i - k >= 0 and j + k < m and j - k >= 0 :
-                    print("Checking for i,j,k ",i,j,k,sep=" ,")
-                    if grid[i+k][j] == 'G' and grid[i-k][j] == 'G' and grid[i][j+k] == 'G' and grid[i][j-k] == 'G':
-                        if k+1 >= max1:
-                            max1 = k+1
-                            max1_i = i
-                            max1_j = j
-                    else:
-                        break
+print(res1)
 
-print("MAX1, i,j",max1,max1_i,max1_j,sep = "---")
+#generate other results:
+
+for result in res1:
+    max_res = result[0]
+    res12.append(result)
+
+    max_res -= 4
+    while max_res > 0:
+        res12.append((max_res, result[1], result[2]))
+        max_res -= 4
 
 
-for k in range(max1):
-    grid[max1_i + k][max1_j] = 'B'
-    grid[max1_i - k][max1_j] = 'B'
-    grid[max1_i][max1_j + k] = 'B'
-    grid[max1_i][max1_j - k] = 'B'
+print(res12)
 
-print(grid)
-for i in range(n):
-    for j in range(m):
-        if grid[i][j] == 'G':
-            for k in range(min(n,m)):
-                if i + k < n and i - k >= 0 and j + k < m and j - k >= 0 :
-                    #print("Checking for i,j,k ",i,j,k,sep=" ,")
-                    if grid[i+k][j] == 'G' and grid[i-k][j] == 'G' and grid[i][j+k] == 'G' and grid[i][j-k] == 'G':
-                        if k+1 >= max2:
-                            max2 = k+1
-                            max2_i = i
-                            max2_j = j
-                    else:
-                        break
+final_result = []
 
+MAT2 = deepcopy(MAT)
+for result in res12:
+    res2 = []
+    size1 = result[0]
+    print("FOR RESULT ", result)
+    cells_to_color = (result[0] + 3)//4
+    row,col = result[1],result[2]
 
-print("MAX2, i,j",max2,max2_i,max2_j,sep = "---")
-area = (max1 * 4 - 3) * (max2 * 4 - 3)
-print(area)
+    print_matrix(MAT2)
+
+    for cell in range(cells_to_color):
+        MAT2[row + cell][col] = 'B'
+        MAT2[row - cell][col] = 'B'
+        MAT2[row][col + cell] = 'B'
+        MAT2[row][col - cell] = 'B'
+
+    print_matrix(MAT2)
+
+    size2 = 0
+    row,col = 0,0
+
+    for i in range(n):
+        for j in range(m):
+            if MAT2[i][j] == 'B':
+                continue
+            res = check_plus(i,j,n,m,MAT1)
+            res2.append(res)
+    print(res2)
+    MAT2 = deepcopy(MAT)
